0c73cb9b4ceb509abfc1028b4ca50d749480f437
Janaki Kandukuri <janaki0424@gmail.com>
2021-01-15 22:26:45 -0600

    Part8 - Added nav bar and interfaces for response data

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { IProduct } from './interface/product';
+import { IPagination } from './interface/pagination';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit {
+  title = 'Skinet';
+  products: IProduct[];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() :void
+  {
+    this.http.get('https://localhost:5001/api/products').subscribe((response: IPagination )=> {
+      this.products = response.data;
+    },
+    error => {
+
+    })
+  }
 }
